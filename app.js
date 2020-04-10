11906f247d794f22c5d462a3b994416cb6c9bf62
unknown <ighotagaddiel98@gmail.com>
2020-04-10 22:56:57 +0100

    myJsSortingAnswer

--- app.js
+++ app.js
@@ -2,3 +2,10 @@
 // write a basic Javascript Program to sort three numbers in asending order
 // example when the user enters 5 , 2 , 6
 // the program will out 2,  5 , 6
+
+function sumNum(a, b, c){
+    let arr = [a, b, c].sort().toString();
+    console.log(arr);
+}
+
+sumNum(5, 2, 6);
