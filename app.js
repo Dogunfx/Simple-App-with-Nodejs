765d59319442de90dfec0d2863260d63b869648e
unknown <ighotagaddiel98@gmail.com>
2020-04-10 23:18:45 +0100

    assignmentCorrection

--- app.js
+++ app.js
@@ -3,9 +3,9 @@
 // example when the user enters 5 , 2 , 6
 // the program will out 2,  5 , 6
 
-function sumNum(a, b, c){
+function sortNum(a, b, c){
     let arr = [a, b, c].sort().toString();
     console.log(arr);
 }
 
-sumNum(5, 2, 6);
+sortNum(5, 2, 6);
