c4c4a713597843492917d58cef7136212334db4f
unknown <ighotagaddiel98@gmail.com>
2020-04-11 02:28:19 +0100

    assignmentUpedate

--- app.js
+++ app.js
@@ -3,8 +3,18 @@
 // example when the user enters 5 , 2 , 6
 // the program will out 2,  5 , 6
 
-function sortNum(a, b, c){
-    let arr = [a, b, c].sort().toString();
+function sortNum(a, b, c) {
+    let arr = [a, b, c]
+    var len = arr.length;
+    for (var i = len - 1; i >= 0; i--) {
+        for (var j = 1; j <= i; j++) {
+            if (arr[j - 1] > arr[j]) {
+                var temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
     console.log(arr);
 }
 
